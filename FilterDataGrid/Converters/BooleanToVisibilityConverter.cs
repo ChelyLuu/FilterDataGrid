ab5cc5702be65113c3fdd4c0fe1acf24c4c6dfa2
ChelyLuu <73624088+ChelyLuu@users.noreply.github.com>
2025-07-28 16:03:15 +0800

    Add files via upload

---
+++ FilterDataGrid/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,18 @@
+﻿using MaterialDesignThemes.Wpf.Converters;
+using System.Windows;
+
+namespace FilterDataGrid.Converters
+{
+    public sealed class BooleanToVisibilityConverter : BooleanConverter<Visibility>
+    {
+        public static readonly BooleanToVisibilityConverter CollapsedInstance = new BooleanToVisibilityConverter() { FalseValue = Visibility.Collapsed, TrueValue = Visibility.Visible };
+        public static readonly BooleanToVisibilityConverter NotCollapsedInstance = new BooleanToVisibilityConverter() { FalseValue = Visibility.Visible, TrueValue = Visibility.Collapsed };
+
+        public static readonly BooleanToVisibilityConverter HiddenInstance = new BooleanToVisibilityConverter() { FalseValue = Visibility.Hidden, TrueValue = Visibility.Visible };
+        public static readonly BooleanToVisibilityConverter NotHiddenInstance = new BooleanToVisibilityConverter() { FalseValue = Visibility.Visible, TrueValue = Visibility.Hidden };
+
+        public BooleanToVisibilityConverter() :
+            base(Visibility.Visible, Visibility.Collapsed)
+        { }
+    }
+}
---
+++ FilterDataGrid/Converters/ColumnVisibleConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FilterDataGrid.Converters
+{
+    public class ColumnVisibleConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            string c = values[1].ToString();
+            if (values[0] == null || c == "") return false;
+            else
+            {
+                ColumnProperty cp = (values[0] as List<ColumnProperty>).Where(x => x.Name == c).FirstOrDefault();
+                if (cp.Visibility == Visibility.Visible) return true;
+                else return false;
+            }
+
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
